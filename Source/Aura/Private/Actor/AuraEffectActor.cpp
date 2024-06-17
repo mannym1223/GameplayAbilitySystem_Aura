76b3ca6e538361af7e0ab87562e11a8e6e2d637a
Manuel Munoz <mannymunoz1223@live.com>
2024-06-16 20:05:22 -0500

    Created effect actor with overlap events.
    Currently, the on overlap event manually adds health to actor with attributes.

---
+++ Source/Aura/Private/Actor/AuraEffectActor.cpp
@@ -0,0 +1,43 @@
+// Copyright Manuel Munoz
+
+
+#include "Actor/AuraEffectActor.h"
+#include "Components/SphereComponent.h"  
+#include <AbilitySystemInterface.h>
+#include <AbilitySystem/AuraAttributeSet.h>
+
+AAuraEffectActor::AAuraEffectActor()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+	SetRootComponent(Mesh);
+
+	Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
+	Sphere->SetupAttachment(GetRootComponent());
+}
+
+void AAuraEffectActor::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	//TODO: Change this to apply gameplay effect. Using const cast as a hack.
+	if (IAbilitySystemInterface* ASCInterface = Cast<IAbilitySystemInterface>(OtherActor)) {
+		const UAuraAttributeSet* attributeSet = Cast<UAuraAttributeSet>(ASCInterface->GetAbilitySystemComponent()->GetAttributeSet(UAuraAttributeSet::StaticClass()));
+		
+		UAuraAttributeSet* mutableAttributeSet = const_cast<UAuraAttributeSet*>(attributeSet);
+		mutableAttributeSet->SetHealth(attributeSet->GetHealth() + 25.f); 
+		Destroy();
+	}
+}
+
+void AAuraEffectActor::EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+}
+
+void AAuraEffectActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	Sphere->OnComponentBeginOverlap.AddDynamic(this, &AAuraEffectActor::OnOverlap);
+	Sphere->OnComponentEndOverlap.AddDynamic(this, &AAuraEffectActor::EndOverlap);
+}
+
