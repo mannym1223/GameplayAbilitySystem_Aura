d09d9081050730c8218a15c29e7a62bd06ae33c7
mannym1223 <mannymunoz1223@live.com>
2024-10-16 19:48:22 -0500

    Added bindings for Mana changed delegates.

--- Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
@@ -20,13 +20,22 @@ void UOverlayWidgetController::BindCallbacksToDependencies()
 {
 	const UAuraAttributeSet* auraSet = CastChecked<UAuraAttributeSet>(AttributeSet);
 
+	// Health bindings
 	AbilitySystem->GetGameplayAttributeValueChangeDelegate(
 		auraSet->GetHealthAttribute()).AddUObject(
 			this, &UOverlayWidgetController::HealthChanged);
-
 	AbilitySystem->GetGameplayAttributeValueChangeDelegate(
 		auraSet->GetMaxHealthAttribute()).AddUObject(
 			this, &UOverlayWidgetController::MaxHealthChanged);
+
+	// Mana bindings
+	AbilitySystem->GetGameplayAttributeValueChangeDelegate(
+		auraSet->GetManaAttribute()).AddUObject(
+			this, &UOverlayWidgetController::ManaChanged);
+	AbilitySystem->GetGameplayAttributeValueChangeDelegate(
+		auraSet->GetMaxManaAttribute()).AddUObject(
+			this, &UOverlayWidgetController::MaxManaChanged);
+	
 }
 
 void UOverlayWidgetController::HealthChanged(const FOnAttributeChangeData& Data) const
@@ -37,4 +46,14 @@ void UOverlayWidgetController::HealthChanged(const FOnAttributeChangeData& Data)
 void UOverlayWidgetController::MaxHealthChanged(const FOnAttributeChangeData& Data) const
 {
 	OnMaxMaxHealthChanged.Broadcast(Data.NewValue);
+}
+
+void UOverlayWidgetController::ManaChanged(const FOnAttributeChangeData& Data) const
+{
+	OnManaChanged.Broadcast(Data.NewValue);
+}
+
+void UOverlayWidgetController::MaxManaChanged(const FOnAttributeChangeData& Data) const
+{
+	OnMaxManaChanged.Broadcast(Data.NewValue);
 }
